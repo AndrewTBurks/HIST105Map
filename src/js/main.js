3fdd91f758e21ad584abf3412291cb5c617fca13
Andrew Burks <andrewtburks@gmail.com>
2017-04-04 11:32:37 -0500

    Progress on overall layout, added countries to translate # to name

--- src/js/main.js
+++ src/js/main.js
@@ -5,68 +5,79 @@ window.addEventListener("resize", function() {
 });
 
 (function() {
-  var width = 960,
-  height = 600;
-
-  let projection = d3.geoCylindricalStereographic()
-
-  // add zoom behavior
-  let zoom = d3.zoom()
-    .scaleExtent([1.246, 20])
-    .translateExtent([[-109.16, -22.6], [width, height]])
-    .on("zoom", zoomed);
-
-    // .clipExtent([[0, 0], [width, height]])
-    // .scale(110)
-    // .center([54.5260, 15.2551]);
-
-  console.log(projection.scale());
-
-  var path = d3.geoPath()
-    .projection(projection)
+  let path, zoom, projection;
 
   App.init = function() {
+    // load country codes first
 
-    App.svg = d3.select("#mainContent")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", "0 0 " + width + " " + height)
-      .call(zoom);
 
-    App.g = App.svg.append("g")
-      .attr("transform", "translate(-109.16, -22.6) scale(1.246)");
+    createMap();
+    createTimeline();
 
-    drawMap();
+
+    function createMap() {
+      App.map = {};
+
+      App.map.SVG = d3.select("#map")
+        .append("svg");
+
+      let width = App.map.SVG.node().clientWidth,
+        height = App.map.SVG.node().clientHeight;
+
+      projection = d3.geoCylindricalStereographic()
+      // .clipExtent([[0, 0], [width, height]]);
+
+      // add zoom behavior
+      zoom = d3.zoom()
+        .scaleExtent([1, 20])
+        .translateExtent([[0, 0], [width, height]])
+        .on("zoom", zoomed);
+
+      path = d3.geoPath()
+      .projection(projection);
+
+
+      App.map.SVG
+        .attr("viewBox", "0 0 " + width + " " + height)
+        // .attr("transform", "translate(109.16, 22.6) scale(1.246)")
+        .call(zoom);
+
+      App.map.countryG = App.map.SVG.append("g")
+
+      drawMap();
+
+      function drawMap() {
+        d3.json("./data/world.json", function(error, json) {
+          let features = topojson.feature(json, json.objects.countries).features;
+
+          console.log(features);
+
+          App.map.countryG.selectAll(".country")
+          .data(features)
+          .enter().append("path")
+          .attr("class", "country")
+          .attr("d", path)
+          .on("click", function(d) {
+            console.log(d);
+          });
+
+
+        });
+      }
+
+      function zoomed() {
+        App.map.countryG.attr("transform", d3.event.transform);
+
+        // make strokes on countries thinner
+        App.map.countryG.selectAll("path")
+        .style("stroke-width", 1/d3.event.transform.k);
+      }
+    }
+
+    function createTimeline() {
+
+    }
+
   };
 
-  function drawMap() {
-    d3.json("./data/world.json", function(error, json) {
-      let features = topojson.feature(json, json.objects.countries).features;
-
-      console.log(features);
-
-      App.g.selectAll(".country")
-        .data(features)
-      .enter().append("path")
-        .attr("class", "country")
-        .attr("d", path)
-        .on("click", function(d) {
-          console.log(d);
-        });
-
-      // App.svg.insert("path", ".graticule")
-      //   .datum(topojson.mesh(json, json.objects.countries, function(a, b) { return a !== b; }))
-      //   .attr("class", "boundary")
-      //   .attr("clip-path", "url(#clip)")
-      //   .attr("d", path);
-    });
-  }
-
-  function zoomed() {
-    console.log(d3.event);
-    App.g.attr("transform", d3.event.transform);
-    App.g.selectAll("path")
-      .style("stroke-width", 1/d3.event.transform.k);
-  }
 })();
